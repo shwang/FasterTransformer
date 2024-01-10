eb95ffc0274a13b796e7117d7986ac1f247e1b3e
wang.steven.h@gmail.com <wang.steven.h@gmail.com>
2024-01-10 14:53:08 +0100

    try disable operator overload bfloat162

--- src/fastertransformer/utils/cuda_bf16_fallbacks.cuh
+++ src/fastertransformer/utils/cuda_bf16_fallbacks.cuh
@@ -169,13 +169,13 @@ inline __device__ __nv_bfloat162 bf16exp2(const __nv_bfloat162 x) {
 }
 
 #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 800)
-inline __device__ __nv_bfloat162 operator*(const __nv_bfloat162 x, const __nv_bfloat162 y) { return bf16hmul2(x, y); };
-inline __device__ __nv_bfloat162 operator+(const __nv_bfloat162 x, const __nv_bfloat162 y) { return bf16hadd2(x, y); };
-
-inline __device__ __nv_bfloat162 make_bfloat162(const __nv_bfloat16 x, const __nv_bfloat16 y)
-{
-    __nv_bfloat162 t; t.x = x; t.y = y; return t;
-}
+// inline __device__ __nv_bfloat162 operator*(const __nv_bfloat162 x, const __nv_bfloat162 y) { return bf16hmul2(x, y); };
+// inline __device__ __nv_bfloat162 operator+(const __nv_bfloat162 x, const __nv_bfloat162 y) { return bf16hadd2(x, y); };
+// 
+// inline __device__ __nv_bfloat162 make_bfloat162(const __nv_bfloat16 x, const __nv_bfloat16 y)
+// {
+//     __nv_bfloat162 t; t.x = x; t.y = y; return t;
+// }
 
 #endif
 
